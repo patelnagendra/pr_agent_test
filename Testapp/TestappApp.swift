2e26ddafcf763c8f6f7bc8d112e269091ae35f6b
Nagendra Patel <nagendrapatel@Nagendras-MacBook-Air.local>
2026-07-30 15:21:52 +0530

    intial commit

---
+++ Testapp/TestappApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestappApp.swift
+//  Testapp
+//
+//  Created by Nagendra Patel on 30/05/26.
+//
+
+import SwiftUI
+
+@main
+struct TestappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
